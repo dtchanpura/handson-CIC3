c87d43a20451213e02ea0201cfa2e48ad0c04b75
Darshil Chanpura <dtchanpura@gmail.com>
2014-08-31 18:00:00 +0530

    Added task1.cpp CPP variant for Task1

---
+++ Task1/task1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main (int argc, char **argv) 
+
+{
+
+char name[20];
+cout << "Enter your Full name:";
+cin >> name;
+cout << "Hello " << name << endl;
+return 0;
+
+}
